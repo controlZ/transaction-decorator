5d76ef1d1bbe0c351430ce17b3792796edff3a85
controlZ <o_dahyun@gm.gist.ac.kr>
2023-10-09 00:25:27 +0900

    ✨ feat: add user, point entities

--- src/global/config/database/database.service.ts
+++ src/global/config/database/database.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmOptionsFactory } from '@nestjs/typeorm';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist';
+import { Point } from '../../../point/entity/point.entity';
+import { User } from '../../../user/entity/user.entity';
 
 @Injectable()
 export class MySQLConfigService implements TypeOrmOptionsFactory {
@@ -14,7 +16,7 @@ export class MySQLConfigService implements TypeOrmOptionsFactory {
       port: this.configService.get<number>('MYSQL_DATABASE_PORT'),
       host: this.configService.get<string>('MYSQL_DATABASE_HOST'),
       database: this.configService.get<string>('MYSQL_DATABASE_NAME'),
-      entities: [],
+      entities: [User, Point],
       synchronize: true,
     };
   }
