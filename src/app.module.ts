daf84520519d0b3395420cb740bf512b8adb2427
controlZ <o_dahyun@gm.gist.ac.kr>
2023-10-09 00:25:48 +0900

    ✨ feat: add user, point module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MySQLConfigModule } from './global/config/database/database.module';
 import { MySQLConfigService } from './global/config/database/database.service';
+import { UserModule } from './user/user.module';
+import { PointModule } from './point/point.module';
 
 @Module({
   imports: [
@@ -14,6 +16,8 @@ import { MySQLConfigService } from './global/config/database/database.service';
       useClass: MySQLConfigService,
       inject: [MySQLConfigService],
     }),
+    UserModule,
+    PointModule,
   ],
 })
 export class AppModule {}
